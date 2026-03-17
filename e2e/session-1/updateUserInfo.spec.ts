cd388b7fd20958474898e4933673e180d36d1931
dleMM-kms <dle@magmutual.com>
2026-03-17 13:43:41 +0700

    put session 1 files into folder

---
+++ e2e/session-1/updateUserInfo.spec.ts
@@ -0,0 +1,19 @@
+import { test, expect } from '@playwright/test';
+
+const firstName = "first"
+const lastName = "last"
+
+test('test', async ({ page }) => {
+  await page.goto('http://localhost:3000/signin');
+
+  await page.getByRole('textbox', { name: 'Username' }).fill('Heath93');
+  await page.getByRole('textbox', { name: 'Password' }).fill('s3cret');
+  await page.locator('[data-test="signin-submit"]').click();
+  await page.locator('[data-test="sidenav-user-settings"]').click();
+  await page.locator('[data-test="user-settings-firstName-input"]').fill(firstName);
+  await page.locator('[data-test="user-settings-lastName-input"]').fill(lastName);
+  await page.locator('[data-test="user-settings-submit"]').click();
+  let newName = firstName + " " + lastName.charAt(0)
+  console.log(newName)
+  await expect(page.locator('[data-test="sidenav-user-full-name"]')).toContainText(newName);
+});
