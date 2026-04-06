efebb36663e1762b7ad924461316fc345063f8e4
dleMM-kms <dle@magmutual.com>
2026-04-07 01:16:10 +0700

    fix issue storageState not working

--- e2e/session-4/ex1.spec.ts
+++ e2e/session-4/ex1.spec.ts
@@ -1,7 +1,7 @@
 import { test } from '../../fixture/fixture'; // Use custom fixtures from auto/fixture
 import { expect } from '@playwright/test';
 
-test.use({ storageState: 'e2e/session-4/.auth' });
+test.use({ storageState: '.auth' });
 
 test('TC-01: session 4 exercise 1', async ({ loginPage }) => {
   await loginPage.goto('/');
